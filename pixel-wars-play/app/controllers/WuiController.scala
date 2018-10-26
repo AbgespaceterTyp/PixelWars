8ccb961d306093ffe701e1447f2f1e6200115a47
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-10-26 13:13:22 +0200

    print game content in pixelwars.scala.html

--- pixel-wars-play/app/controllers/WuiController.scala
+++ pixel-wars-play/app/controllers/WuiController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 import de.htwg.se.msiwar.aview.MainApp.tui
 import de.htwg.se.msiwar.aview.MainApp.controller
@@ -9,28 +8,13 @@ import de.htwg.se.msiwar.aview.MainApp.controller
 @Singleton
 class WuiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.pixelwars())
+    Ok(views.html.pixelwars(controller))
   }
 
   def command(line: String) = Action{
     tui.executeCommand(line)
 
-    val result = StringBuilder.newBuilder
-    for (i <- 0 until controller.rowCount) {
-      result.append("| ")
-      for (j <- 0 until controller.columnCount) {
-        result.append(controller.cellContentToText(i, j) + " | ")
-      }
-      result.append("\n")
-    }
-    Ok(result.toString())
+    Ok(views.html.pixelwars(controller))
   }
 }

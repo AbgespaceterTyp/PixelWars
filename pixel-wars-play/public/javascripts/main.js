58bfe29ea4f4b167c45211af9d5aca10489e6dd3
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-11-14 13:39:03 +0100

    resize gameboard on level change dynamically

--- pixel-wars-play/public/javascripts/main.js
+++ pixel-wars-play/public/javascripts/main.js
@@ -1,16 +1,22 @@
-function updateGameBoard(rowCount, colCount)
+function updateGameBoard()
 {
     var gameboard = document.getElementById("gameboard");
+    var rowCount = gameboard.getElementsByClassName("gameboardrow").length;
+    var colCount = gameboard.getElementsByClassName("gameboardrow").item(0).getElementsByClassName("gameboardcolumn").length
 
-    gameboard.style.height = "" + 60 * rowCount + "px";
-    gameboard.style.minHeight = "" + 60 * rowCount + "px";
+    gameboard.style.height = 60 * rowCount + "px";
+    gameboard.style.minHeight = 60 * rowCount + "px";
 
-    gameboard.style.width = "" + 60 * colCount + "px";
-    gameboard.style.minWidth = "" + 60 * colCount + "px";
+    gameboard.style.width = 60 * colCount + "px";
+    gameboard.style.minWidth = 60 * colCount + "px";
+
+    gameboard.style.display = "block";
+    gameboard.style.marginLeft = "auto";
+    gameboard.style.marginRight = "auto";
+
+    //gameboard.style.backgroundImage = jsRoutes.controllers.WuiController.javascriptRoutes("../images/background_woodlands.png" ).url
 }
 
 $( document ).ready(function() {
-    console.log( "setting gameboard size" );
-
-    updateGameBoard(12,12)
+    updateGameBoard()
 });

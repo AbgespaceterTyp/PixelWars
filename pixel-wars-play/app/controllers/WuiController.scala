b8398d1c91d11e24a546a6d8a2087211296937cf
Samantha Isted <sa431ist@htwg-konstanz.de>
2018-11-30 13:37:10 +0100

    added method cellsInRange to routes and WuiController

--- pixel-wars-play/app/controllers/WuiController.scala
+++ pixel-wars-play/app/controllers/WuiController.scala
@@ -1,9 +1,11 @@
 package controllers
 
 import javax.inject._
+
 import play.api.mvc._
 import de.htwg.se.msiwar.aview.MainApp.tui
 import de.htwg.se.msiwar.aview.MainApp.controller
+import play.api.libs.json.Json
 
 @Singleton
 class WuiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -11,13 +13,16 @@ class WuiController @Inject()(cc: ControllerComponents) extends AbstractControll
   def index() = Action {
     Ok(views.html.index(controller))
   }
+
   def about() = Action {
     Ok(views.html.about(controller))
   }
+
   def control() = Action {
     Ok(views.html.control(controller))
   }
-  def command(line: String) = Action{
+
+  def command(line: String) = Action {
     tui.executeCommand(line)
     Ok(views.html.pixelwars(controller))
   }
@@ -26,12 +31,17 @@ class WuiController @Inject()(cc: ControllerComponents) extends AbstractControll
     Ok(controller.levelBackgroundImagePath)
   }
 
-  def executeAction(actionId:Int, rowIndex:Int, columnIndex:Int) = Action {
-    controller.executeAction(actionId, rowIndex, columnIndex);
+  def executeAction(actionId: Int, rowIndex: Int, columnIndex: Int) = Action {
+    controller.executeAction(actionId, rowIndex, columnIndex)
     Ok(views.html.pixelwars(controller))
   }
 
-  def canExecuteAction(actionId:Int, rowIndex:Int, columnIndex:Int) = Action {
-    Ok(controller.canExecuteAction(actionId,rowIndex,columnIndex).toString);
+  def canExecuteAction(actionId: Int, rowIndex: Int, columnIndex: Int) = Action {
+    Ok(controller.canExecuteAction(actionId, rowIndex, columnIndex).toString)
+  }
+
+  def cellsInRange(actionId: Int) = Action {
+    val cells = controller.cellsInRange(Option(actionId))
+    Ok(Json.toJson(cells))
   }
 }

c4482645025454a2b21ac9d855aad80a3c39f2c6
Samantha Isted <sa431ist@htwg-konstanz.de>
2018-11-30 12:36:11 +0100

    added Json

---
+++ pixel-wars-play/app/models/JsonConverter.scala
@@ -0,0 +1,9 @@
+package models
+
+import de.htwg.se.msiwar.model.{BlockObject, GameObject}
+import play.api.libs.json.Json
+
+//object JsonConverter {
+//  implicit def block = Json.format[BlockObject]
+//  implicit def gameObj = Json.format[GameObject]
+//}

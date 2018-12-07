69225ac31675e7392da7976e7bc902e6db9255d9
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-12-07 05:39:37 +0100

    added json converter for list of tuples

--- pixel-wars-play/app/models/JsonConverter.scala
+++ pixel-wars-play/app/models/JsonConverter.scala
@@ -4,6 +4,19 @@ import de.htwg.se.msiwar.model._
 import play.api.libs.json._
 
 object JsonConverter {
+  
+  implicit def tuple = new Writes[(Int, Int)] {
+    override def writes(o: (Int, Int)) = Json.obj(
+      "rowIdx" -> o._1,
+      "columnIdx" -> o._2,
+    )
+  }
+
+  implicit def tuples = new Writes[List[(Int, Int)]] {
+    override def writes(o: List[(Int, Int)]): JsValue = {
+      JsArray(o.map(Json.toJson(_)))
+    }
+  }
 
   implicit def gameObject = new Writes[GameObject] {
     def writes(playerObject: GameObject) = Json.obj(

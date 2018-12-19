75ba8c3e4d9f64991b83a9c7cb538deefc740b80
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-12-19 12:12:19 +0100

    moved css properties for status bar to web component

--- pixel-wars-play/public/polymer/status-bar.js
+++ pixel-wars-play/public/polymer/status-bar.js
@@ -27,10 +27,21 @@ class StatusBar extends LitElement {
 
     render() {
         return html `
+        <style>
+            #statusBar {margin-top: 2%; text-align: center;}
+            .hp {color: red; border: red; width: ${this.hp * 32}px; height: 32px; 
+            background-image: url("../images/action_attack.png"); background-repeat: repeat; display: inline-block;}
+            .hp-empty {color: black; border: red; width: ${((this.hp - this.maxHp) * 32) + 1}px; height: 32px; 
+            background-image: url("../images/action_wait.png");  background-repeat: repeat; display: inline-block;}
+            .ap {color: yellow; border: yellow; width: ${this.ap * 32}px; height: 32px; background-repeat: repeat; 
+            background-image: url("../images/action_move.png"); background-repeat: repeat; display: inline-block;}
+            .ap-empty {color: black; border: yellow; width: ${((this.ap - this.maxAp) * 32) + 1}px; height: 32px; 
+            background-image: url("../images/action_wait.png"); background-repeat: repeat; display: inline-block;}
+        </style>
         <div id="statusBar">
             <span id="activePlayer">Turn: ${this.player}</span>
-            <span id="activePlayerHealth">HP: ${this.hp} of ${this.maxHp}</span>
-            <span id="activePlayerAP">AP: ${this.ap} of ${this.maxAp}</span>
+            <span id="activePlayerHealth">HP: </span> <span class="hp"></span> <span class="hp-empty"></span>
+            <span id="activePlayerAP">AP: </span> <span class="ap"></span> <span class="ap-empty"></span>
         </div>
         `;
     }
